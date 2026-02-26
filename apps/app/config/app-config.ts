133cd5353dca50c4d805c03b9f37413ff9d4ca60
lazlanrafar <lazlanrafar@gmail.com>
2026-02-26 10:24:37 +0800

    feat: Update app icons and display names across iOS and Android platforms, and add environment configuration.

--- apps/app/config/app-config.ts
+++ apps/app/config/app-config.ts
@@ -3,12 +3,12 @@ import packageJson from "../package.json";
 const currentYear = new Date().getFullYear();
 
 export const APP_CONFIG = {
-  name: "Studio Admin",
+  name: "Okane - Money Manager",
   version: packageJson.version,
-  copyright: `© ${currentYear}, Studio Admin.`,
+  copyright: `© ${currentYear}, Okane - Money Manager.`,
   meta: {
-    title: "Studio Admin - Modern Next.js Dashboard Starter Template",
+    title: "Okane - Money Manager",
     description:
-      "Studio Admin is a modern, open-source dashboard starter template built with Next.js 16, Tailwind CSS v4, and shadcn/ui. Perfect for SaaS apps, admin panels, and internal tools—fully customizable and production-ready.",
+      "Okane is a modern, privacy-first personal finance tracker built to help you take control of your wealth. Manage your budgets, track your expenses, and achieve your financial goals with ease.",
   },
 };
